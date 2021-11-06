0b5c0c44cd2e30e2114b077cbc8eb5d06f9376b3
Morty <93643714+MortyCPP@users.noreply.github.com>
2021-11-06 21:29:11 +0800

    Update main.cpp

--- 单链表设计/main.cpp
+++ 单链表设计/main.cpp
@@ -13,5 +13,6 @@ int main(int argc, const char * argv[]) {
     for (int i=0; i<myLinkedList->size; i++) {
         cout<<myLinkedList->get(i)<<" "<<endl;
     }
+    //以上为测试代码控制台输出结果应为：1 2 3 4 5
     return 0;
 }
